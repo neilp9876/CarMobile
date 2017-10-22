32c25918398b2a78f0781cbae9eb9790369a8bb9
neilp9876 <neilpartington@gmail.com>
2017-10-22 17:54:28 +0100

    Added steering

--- App/Containers/MainScreen.js
+++ App/Containers/MainScreen.js
@@ -9,8 +9,17 @@ import apisauce from 'apisauce'
 import styles from './Styles/MainScreenStyles'
 import {LightButton, ArrowButton} from './Buttons'
 
+
 export default class LaunchScreen extends Component {
+  steerActionEnum = {
+      NEUTRAL: 1,
+      CENTERING: 2,
+      LEFT: 3,
+      RIGHT: 4
+  }
   api={}
+  steeringAction = this.steerActionEnum.NEUTRAL;
+  steeringDirection = 5; // Scale of 1 to 9
 
   // CommandEnum = Object.freeze({Forward:1, Reverse:2, Right:3, Left:4 })
   
@@ -33,39 +42,141 @@ export default class LaunchScreen extends Component {
     this.state = {
         gearPosition: 1
     }
-   // this.props.commands = {Forward:1, Reverse:2, Right:3, Left:4 }
-
-   // this.state = {
-     // commands: {Forward:1, Reverse:2, Right:3, Left:4 }
-   // }
   }
 
-  SendCommand = (buttonName, state) => {
-    console.log(buttonName + " pressed " + (state == 1 ? 'ON' : 'OFF'))
-    var stateText = (state == 1 ? "ON" : "OFF");
-    var url = buttonName + '?' + stateText;
-    console.log(url);
-    
-    // Call the url and wait for the result
-    this.api.get(url)
-      .then(response => {
-        if (response.ok === false)
-          alert("Web call failed : " + response.problem);
-        else {
-          console.log('State set on Pi');
+  componentDidMount() {
+        console.log('component mounted - starting timer');
+        setInterval(this.PerformSteer, 100);
+  }
 
-          currentGear = this.state.gearPosition;
-          if (url == "Gear_up?OFF") {
-            newGear = currentGear + 1;
-            this.setState({gearPosition: (newGear > 3 ? 3 : newGear)})
-          }
-          else if (url == "Gear_down?OFF") {
-            newGear = currentGear - 1;
-            this.setState({gearPosition: (newGear < 1 ? 1 : newGear)})
-          }
+    SendCommand = (buttonName, state) => {
+        var url = "";
+
+        if (buttonName == "Gear_up" || buttonName == "Gear_down")
+        {
+            if (state == 1)
+                return;
+            url = this.HandleGearChange(buttonName, state);
         }
-      });
+        else
+        {
+            if (buttonName == "Right" || buttonName == "Left") {
+                this.HandleSteering(buttonName, state);
+            }
+            
+            // Do the le.log(buttonName + " pressed " + (state == 1 ? 'ON' : 'OFF'))
+            var stateText = (state == 1 ? "ON" : "OFF");
+            url = buttonName + '?' + stateText;
+        }
+
+        this.PostMessage(url);
     }
+
+    PostMessage = (url) => {
+        console.log(url);
+
+        this.api.get(url)
+            .then(response => {
+                if (response.ok === false)
+                    alert("Web call failed : " + response.problem);
+                else {
+                    console.log('State set on Pi');
+                }
+            });
+    }
+
+    HandleGearChange = (buttonName, state) => {
+        currentGear = this.state.gearPosition;
+
+        if (buttonName == "Gear_up") {
+            newGear = currentGear + 1;
+            if (newGear > 3)
+                newGear = 3;
+            this.setState({gearPosition: (newGear)})
+        }
+        else if (buttonName == "Gear_down") {
+            newGear = currentGear - 1;
+            if (newGear < 1)
+                newGear = 1;
+            this.setState({gearPosition: (newGear)})
+        }
+        url = "Gear?" + newGear;
+
+        return url;
+    }
+
+  HandleSteering = (buttonName, state) => {
+        // We need a timer to keep sending the signals to steer constantly
+        if (state == 0)
+        {
+            // OFF - So return to centre
+            this.steeringAction = this.steerActionEnum.CENTERING;
+            console.log('Steering is now centering');            
+        }
+        else {
+            // Steering - so set the direction
+            if (buttonName == "Left") {
+                this.steeringAction = this.steerActionEnum.LEFT;
+                this.steeringDirection -= 1;
+                console.log('Steering Left');                
+            }
+            else {
+                this.steeringAction = this.steerActionEnum.RIGHT;      
+                this.steeringDirection += 1;
+                console.log('Steering Right');                
+            }
+        }
+        this.PostMessage(this.BuildSteeringUrl());
+    }
+
+    PerformSteer = () => {
+        
+        if (this.steeringAction == this.steerActionEnum.NEUTRAL)
+            return;
+
+        console.log('Steering Update...');                
+            
+        // Otherwise we need to steer...
+        switch(this.steeringAction)
+        {
+            case this.steerActionEnum.CENTERING:
+                // Change the direction towards the neutral
+                if (this.steeringDirection > 5)
+                    this.steeringDirection -= 1;
+                else if (this.steeringDirection < 5)
+                    this.steeringDirection += 1;
+                
+                // Check whether we have now centred
+                if (this.steeringDirection == 5) {
+                    // Its centred so set the action so we stop
+                    this.steeringAction = this.steerActionEnum.NEUTRAL;
+                }
+                break;
+
+            case this.steerActionEnum.LEFT:
+                this.steeringDirection -= 1;
+                if (this.steeringDirection < 1)
+                    this.steeringDirection = 1;
+                break;
+
+            case this.steerActionEnum.RIGHT:
+                this.steeringDirection += 1;
+                if (this.steeringDirection > 9)
+                    this.steeringDirection = 9;  
+                break;       
+        }
+
+        console.log('Steering direction = ' + this.steeringDirection);                
+        
+        // Get the URL and send it 
+        url = this.BuildSteeringUrl();
+        this.PostMessage(url);
+    }
+
+    BuildSteeringUrl = () => {
+        url = "Steer?" + this.steeringDirection;
+        return url;
+    } 
 
   //flex-start, center, flex-end, space-around, and space-between
   render () {
